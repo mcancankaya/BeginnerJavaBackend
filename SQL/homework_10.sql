95a4abd85203e70201495b65fbede51c67380bb3
mcancankaya <mccankaya45@hotmail.com>
2023-12-13 15:24:29 +0300

    SQL Homework 10 add.

---
+++ SQL/homework_10.sql
@@ -0,0 +1,14 @@
+/*
+	1. city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+	2. customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+	3. customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+*/
+
+-- 1.
+SELECT city, country FROM city LEFT JOIN country ON city.country_id = country.country_id;
+
+-- 2.
+SELECT c.first_name, c.last_name FROM customer AS c RIGHT JOIN payment AS p ON c.customer_id = p.customer_id;
+
+-- 3.
+SELECT c.first_name, c.last_name FROM customer AS c FULL JOIN rental ON rental.customer_id = c.customer_id;
